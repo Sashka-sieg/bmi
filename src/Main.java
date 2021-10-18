c89b963f2075d5350f743f00deb79e1c07641e59
Alexandr Yakushin <valensiec08@mail.ru>
2021-10-18 21:17:55 +0300

    edit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-                int weight = 50;
-                double height = 1.80;
-               double bmi =service.calculate(weight,height);
+        int weight = 50;
+        double height = 1.80;
+        double bmi = service.calculate(weight, height);
         System.out.println(bmi);
     }
 }
